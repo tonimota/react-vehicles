f8c3965895bd713e2e5fd134e41bb28a600e2e74
Tonielton Mota <toni.elton1993@gmail.com>
2019-04-20 20:01:00 -0300

    [FEAT] Create router and include bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route} from 'react-router'
+import VehiclesContainer from './pages/Vehicles'
+// import { Container, Title } from "bloomer";
+import {Container} from 'react-bootstrap';
+import 'bootstrap/scss/bootstrap.scss'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Container fluid={true}>
+        <h1>Vehicles</h1>
+        <Switch>
+          <Route path="/" exact={true} component={VehiclesContainer} />
+        </Switch>
+      </Container>
+    )
   }
 }
 
